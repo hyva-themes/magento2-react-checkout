58d6ecbb00204f9783ba307e172063aa875ee601
Willem Wigman <wigman@users.noreply.github.com>
2020-10-13 21:20:59 +0200

    Create registration.php

---
+++ src/registration.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Hyvä Themes - https://hyva.io
+ * Copyright © Wigman Interactive. All rights reserved.
+ * This product is licensed per Magento production install
+ */
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Hyva_Checkout', __DIR__);
