f2e1c7e73ca3f61e280c9aeb2eff7e26a58688af
Rajeev K Tomy <rajeevphpdeveloper@gmail.com>
2020-12-26 16:06:44 +0530

    [BEAUTIFY]use common UI components wherever possible

--- src/reactapp/src/components/Elements/BillingAddress.jsx
+++ src/reactapp/src/components/Elements/BillingAddress.jsx
@@ -1,129 +1,81 @@
-import React, { useState } from 'react';
-import ArrowUpIcon from '../Icons/ArrowUp';
-import ArrowDownIcon from '../Icons/ArrowDown';
+import React from 'react';
 import { FormikProvider, Form } from 'formik';
-
-import { TextInput } from './FormUI/TextInput';
 
 import { useFormikContext } from '../../context/Formik';
 import { useAppContext } from '../../context/App';
 import * as appStates from '../../context/actions/actionStates';
-import {RegularSelect} from "./FormUI/RegularSelect";
+import { RegularSelect } from './FormUI/RegularSelect';
+import ToggleBox from '../Common/ToggleBox';
+import TextInput from '../Common/Form/TextInput';
 
 export const BillingAddress = () => {
+  const [{ BillingAddressForm }] = useFormikContext();
 
-    const [{ BillingAddressForm }] = useFormikContext();
+  const [{ countries, billingOpen }, { fetchCountries }] = useAppContext();
 
-    const [{ countries, billingOpen }, { fetchCountries, setBillingOpen }] = useAppContext();
+  React.useEffect(() => {
+    if (
+      (!countries.data || !countries.data.length) &&
+      countries.state !== appStates.PENDING
+    ) {
+      fetchCountries();
+    }
+  }, [countries, fetchCountries]);
 
-    React.useEffect(() => {
-        if (
-            (!countries.data || !countries.data.length) &&
-            countries.state !== appStates.PENDING
-        ) {
-            fetchCountries();
-        }
-    }, [countries, fetchCountries]);
-
-    return (
-        <div className="border-t border-b py-4">
-            <header
-                className="flex justify-between items-center cursor-pointer select-none"
-                onClick={() => setBillingOpen(!billingOpen)}
-            >
-                <span className="text-indigo font-thin text-xl">Billing information</span>
-                <div className="flex items-center justify-center">
-                    {billingOpen && <ArrowUpIcon size={16} class={'h-5 w-5'} />}
-                    {!billingOpen && (
-                        <ArrowDownIcon size={16} class={'h-5 w-5'} />
-                    )}
-                </div>
-            </header>
-            {billingOpen && (
-                <div className={'py-2'}>
-                    <FormikProvider value={BillingAddressForm}>
-                        <Form>
-                            <TextInput
-                                label="Company"
-                                id="company"
-                                name="company"
-                                helpText=""
-                                type="text"
-                                className={'form-input w-full'}
-                                placeholder="Company"
-                                required={true}
-                            />
-                            <TextInput
-                                label="Firstname"
-                                id="firstname"
-                                name="firstname"
-                                helpText=""
-                                type="text"
-                                className={'form-input w-full'}
-                                placeholder="Firstname"
-                                required={true}
-                            />
-                            <TextInput
-                                label="Lastname"
-                                id="lastname"
-                                name="lastname"
-                                helpText=""
-                                type="text"
-                                className={'form-input w-full'}
-                                placeholder="Lastname"
-                                required={true}
-                            />
-                            <TextInput
-                                label="Street, Nr"
-                                id="street"
-                                name="street"
-                                helpText=""
-                                type="text"
-                                className={'form-input w-full'}
-                                placeholder="Street"
-                                required={true}
-                            />
-                            <TextInput
-                                label="Postal Code"
-                                id="zipcode"
-                                name="zipcode"
-                                helpText=""
-                                type="text"
-                                className={'form-input w-full'}
-                                placeholder="12345"
-                                required={true}
-                            />
-                            <TextInput
-                                label="City"
-                                id="city"
-                                name="city"
-                                helpText=""
-                                type="text"
-                                className={'form-input w-full'}
-                                placeholder="City"
-                                required={true}
-                            />
-                            <RegularSelect
-                                label="Country"
-                                id="country"
-                                name="country"
-                                required={true}
-                                options={countries.data}
-                            />
-                            <TextInput
-                                label="Phone"
-                                id="phone"
-                                name="phone"
-                                helpText=""
-                                type="text"
-                                className={'form-input w-full'}
-                                placeholder="+32 000 000 000"
-                                required={true}
-                            />
-                        </Form>
-                    </FormikProvider>
-                </div>
-            )}
+  return (
+    <ToggleBox title="Billing information" show>
+      {billingOpen && (
+        <div className="py-2">
+          <FormikProvider value={BillingAddressForm}>
+            <Form>
+              <TextInput
+                label="Company"
+                name="company"
+                placeholder="Company"
+                required
+              />
+              <TextInput
+                label="Firstname"
+                name="firstname"
+                placeholder="Firstname"
+                required
+              />
+              <TextInput
+                label="Lastname"
+                name="lastname"
+                placeholder="Lastname"
+                required
+              />
+              <TextInput
+                label="Street, Nr"
+                name="street"
+                placeholder="Street"
+                required
+              />
+              <TextInput
+                label="Postal Code"
+                name="zipcode"
+                placeholder="12345"
+                required
+              />
+              <TextInput label="City" name="city" placeholder="City" required />
+              <RegularSelect
+                label="Country"
+                id="country"
+                name="country"
+                required
+                options={countries.data}
+              />
+              <TextInput
+                label="Phone"
+                name="phone"
+                placeholder="+32 000 000 000"
+                required
+              />
+            </Form>
+          </FormikProvider>
         </div>
-    );
+      )}
+    </ToggleBox>
+  );
 };
--- src/reactapp/src/index.jsx
+++ src/reactapp/src/index.jsx
@@ -2,18 +2,15 @@ import 'react-app-polyfill/ie11';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Main as MainLayout } from './components/Layouts/Main';
-import ContextProvider from "./context/ContextProvider";
+import ContextProvider from './context/ContextProvider';
 
-class Checkout extends React.Component {
-    render() {
-        return (
-            <ContextProvider>
-                <MainLayout />
-            </ContextProvider>
-        );
-    }
+function Checkout() {
+  return (
+    <ContextProvider>
+      <MainLayout />
+    </ContextProvider>
+  );
 }
-
-let RootElement = document.getElementById('react-checkout');
+const RootElement = document.getElementById('react-checkout');
 
 ReactDOM.render(<Checkout />, RootElement);
