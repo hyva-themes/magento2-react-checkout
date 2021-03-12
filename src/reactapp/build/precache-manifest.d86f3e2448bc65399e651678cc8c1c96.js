40ae9178acd4da26cbaca2cb1d8a95df70c2b5e8
Rajeev K Tomy <rajeevphpdeveloper@gmail.com>
2021-03-12 12:24:23 +0530

    [BUILD]updating checkout build js file

---
+++ src/reactapp/build/precache-manifest.d86f3e2448bc65399e651678cc8c1c96.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b2c17dc504fb8d3adf8",
+    "url": "/../../view/frontend/web/js/react-checkout.js"
+  },
+  {
+    "revision": "d4b9583852996254d9a5b908c50798f8",
+    "url": "/../../view/frontend/web/js/react-checkout.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a5fdeb11444fe809d942231ef381b80",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3b2c17dc504fb8d3adf8",
+    "url": "/static/css/main.1d91bb1d.css"
+  }
+]);
--- src/reactapp/build/service-worker.js
+++ src/reactapp/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.82422a8c3ec15dc880eaf2c28551ca7a.js"
+  "/precache-manifest.d86f3e2448bc65399e651678cc8c1c96.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
