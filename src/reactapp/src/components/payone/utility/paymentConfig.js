9fe859f9619b8bf7b48325805ebae3d170992753
ilanbaron <baron.ilan@gmail.com>
2021-05-29 20:40:20 -0300

    Payone creditcard config

--- src/reactapp/src/components/payone/utility/paymentConfig.js
+++ src/reactapp/src/components/payone/utility/paymentConfig.js
@@ -10,7 +10,7 @@ const inputStyles =
 const selectStyles =
   'width:100%;border: 1px solid #e2e8f0; padding: .5rem .75rem;margin:0;min-height:24px;border-radius:0.25rem;font-size: 1rem;line-height: 1.5;';
 const iframe = { height: '50px', width: '100%' };
-const payOne = config.payone;
+const payOne = config.payment.payone;
 const {
   hostedRequest,
   fieldConfig,
