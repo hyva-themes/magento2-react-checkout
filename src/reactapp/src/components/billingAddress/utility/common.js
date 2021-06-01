bf36ab76df3c6176031d62c6339658b2f77d6f32
Rajeev K Tomy <rajeevphpdeveloper@gmail.com>
2021-06-01 11:52:37 +0530

    removing unwanted utility files

--- src/reactapp/src/components/billingAddress/utility/common.js
+++
@@ -1,3 +0,0 @@
-export const CART_BILLING_ADDRESS = 'cart_billing_address';
-export const MY_CART_NEW_ADDRESS = `my_${CART_BILLING_ADDRESS}`;
-export const GUEST_CART_NEW_ADDRESS = CART_BILLING_ADDRESS;
