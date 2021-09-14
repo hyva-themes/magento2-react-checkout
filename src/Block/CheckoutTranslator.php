81000f48c9fff21953788d98009fbe9a54c3f95b
Rajeev K Tomy <rajeevphpdeveloper@gmail.com>
2021-09-14 12:27:00 +0530

    apply translation via layout xml

---
+++ src/Block/CheckoutTranslator.php
@@ -0,0 +1,31 @@
+<?php
+declare(strict_types=1);
+
+namespace Hyva\Checkout\Block;
+
+
+use Magento\Framework\View\Element\Template;
+
+class CheckoutTranslator extends Template
+{
+    public function getTranslations(): string
+    {
+        $translations = $this->getCheckoutTranslations();
+
+        if (!isset($translations)) {
+            return json_encode([]);
+        }
+
+        $stringsTranslated = [];
+
+        foreach ($translations as $translatableItem) {
+            $stringCollection  = explode(',', $translatableItem);
+
+            foreach ($stringCollection as $stringToBeTranslated) {
+                $stringsTranslated[$stringToBeTranslated] = __($stringToBeTranslated);
+            }
+        }
+
+        return json_encode($stringsTranslated);
+    }
+}
