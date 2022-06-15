6729aec846e116b2121a088d5b1a10f5683bcbde
wigman <info@willemwigman.nl>
2022-06-15 15:46:45 +0200

    #292 Add support for automatic tailwind config merge

---
+++ src/Observer/RegisterModuleForHyvaConfig.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Hyvä Themes - https://hyva.io
+ * Copyright © Hyvä Themes 2020-present. All rights reserved.
+ * This product is licensed per Magento install
+ * See https://hyva.io/license
+ */
+
+declare(strict_types=1);
+
+namespace Hyva\ReactCheckout\Observer;
+
+use Magento\Framework\Component\ComponentRegistrar;
+use Magento\Framework\Event\Observer;
+use Magento\Framework\Event\ObserverInterface;
+
+class RegisterModuleForHyvaConfig implements ObserverInterface
+{
+    /**
+     * @var ComponentRegistrar
+     */
+    private $componentRegistrar;
+
+    public function __construct(ComponentRegistrar $componentRegistrar)
+    {
+        $this->componentRegistrar = $componentRegistrar;
+    }
+
+    public function execute(Observer $event)
+    {
+        $config = $event->getData('config');
+        $extensions = $config->hasData('extensions') ? $config->getData('extensions') : [];
+
+        $moduleName = implode('_', array_slice(explode('\\', __CLASS__), 0, 2));
+
+        $path = $this->componentRegistrar->getPath(ComponentRegistrar::MODULE, $moduleName);
+
+        // Only use the path relative to the Magento base dir
+        $extensions[] = ['src' => substr($path, strlen(BP) + 1)];
+
+        $config->setData('extensions', $extensions);
+    }
+}
