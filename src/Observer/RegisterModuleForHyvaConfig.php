ff62d1ed6be68b3a302160a00e440cfcb990f75d
Willem Wigman <wigman@users.noreply.github.com>
2022-06-16 09:53:38 +0200

    #292 Remove copyright notice

--- src/Observer/RegisterModuleForHyvaConfig.php
+++ src/Observer/RegisterModuleForHyvaConfig.php
@@ -1,11 +1,4 @@
 <?php
-/**
- * Hyvä Themes - https://hyva.io
- * Copyright © Hyvä Themes 2020-present. All rights reserved.
- * This product is licensed per Magento install
- * See https://hyva.io/license
- */
-
 declare(strict_types=1);
 
 namespace Hyva\ReactCheckout\Observer;
